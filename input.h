3bd94d66e6bf2c92bcac29dfd1e46e64c81b662d
Kunanta0 <faxim2020@yandex.ru>
2026-08-21 00:25:59 +0400

    Update input.h

--- input.h
+++ input.h
@@ -1,1 +1,26 @@
+#ifndef INPUT_H_INCLUDED
+#define INPUT_H_INCLUDED
 
+double asknum(char);
+//запрашивает коэффициенты квадратного уравнения
+double asknum(char coeff)
+{
+    double n = 0;
+    printf("Введите коэффициент %c: ", coeff);
+    char ch = '\0';
+    bool notok = true;
+
+    while(notok)
+    {
+        int cor = scanf("%lg", &n);
+        if (cor != 1 || (ch = getchar()) != '\n')
+        {
+            while ((ch = getchar()) != '\n') continue;
+            printf("Неправильный ввод, введите число: ");
+        }
+        else notok = false;
+    }
+    return n;
+}
+
+#endif // INPUT_H_INCLUDED
