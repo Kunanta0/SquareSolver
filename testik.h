39c6c8e8efba2bb1d3ba63b95e6cf974b2c00134
Kunanta0 <faxim2020@yandex.ru>
2026-08-21 21:29:42 +0400

    Update testik.h

--- testik.h
+++ testik.h
@@ -1,35 +1,15 @@
 #ifndef TESTIK_H_INCLUDED
 #define TESTIK_H_INCLUDED
 
-#include "maths.h"
-
 bool test(struct coeffs, struct ans);
 
 //проверяет ответ
 bool test(struct coeffs EQ, struct ans otvet)
 {
-    switch (otvet.id)
-    {
-        case 0:
-            if (test_kvad0(EQ)) break;
-            return false;
-        case 1:
-            if (test_kvad1(EQ, otvet.x1)) break;
-            return false;
-        case 2:
-            if (test_kvad2(EQ, otvet.x1, otvet.x2)) break;
-            return false;
-        case 3:
-            if (test_kvad3(EQ, otvet.x1)) break;
-            return false;
-        case 4:
-            if (test_kvad4(EQ)) break;
-            return false;
-        case 5:
-            if (test_kvad5(EQ)) break;
-            return false;
-    }
-    return true;
+    int id0 = num_sol(EQ);
+    struct ans otvetik = answer(EQ, id0);
+    if ((otvet.id == otvetik.id) && (otvet.x1 == otvetik.x1) && (otvet.x2 == otvetik.x2)) return true;
+    return false;
 }
 
 #endif // TESTIK_H_INCLUDED
