325240cb7f02bc85ce10c4001faca49e7289822a
Kunanta0 <faxim2020@yandex.ru>
2026-08-21 14:56:05 +0400

    Update myassert.h

--- myassert.h
+++ myassert.h
@@ -1,19 +1,13 @@
 #ifndef MYASSERT_H_INCLUDED
 #define MYASSERT_H_INCLUDED
 
-void MyAssert(bool);
-
-
 //свой assert()
-void MyAssert(bool exp)
-{
-    if (exp) exit(0);
-    else
-    {
-        printf("Ошибка в файле %s, в строке %d, в функции %s\n", __FILE__, __LINE__, __func__);
-        exit(1);
-    }
-}
-
+#define MyAssert(exp)\
+if (exp) exit(0);\
+else\
+{\
+    fprintf(stderr, "Ошибка: %s в файле %s, в строке %d, в функции %s\n",#exp, __FILE__, __LINE__, __func__);\
+    exit(1);\
+}\
 
 #endif // MYASSERT_H_INCLUDED
