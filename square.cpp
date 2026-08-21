0cc4efb284f364601d44425673fc4d1ec09b293c
Kunanta0 <faxim2020@yandex.ru>
2026-08-21 14:55:10 +0400

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 #include <math.h>
 #include "input.h"
@@ -7,6 +8,8 @@
 
 int main(void)
 {
+    const char* s = (test_kvad(1, -5, 6, 2, 3)) ? "true" : "false";
+    printf("%s\n", s);
 
     setlocale(LC_ALL, "Russian");
 
@@ -16,7 +19,7 @@ int main(void)
     {
         printf("Квадратное уравнение имеет вид ax^2 + bx + c = 0\n");
 
-        double a = 0, b = 0, c = 0;
+        double a, b, c = 0;
         a = asknum('a');
         b = asknum('b');
         c = asknum('c');
@@ -29,6 +32,8 @@ int main(void)
         check = menu();
     }
     while (check == 1);
+
+    MyAssert(check == 1);
 
     printf("Пока!");
     return 0;
