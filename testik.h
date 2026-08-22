9682541e31bfb92ed09fa9497f39532a0c373049
Kunanta0 <faxim2020@yandex.ru>
2026-08-22 12:56:06 +0300

    Update testik.h

--- testik.h
+++ testik.h
@@ -1,15 +1,46 @@
 #ifndef TESTIK_H_INCLUDED
 #define TESTIK_H_INCLUDED
 
-bool test(struct coeffs, struct ans);
+void test();
 
 //проверяет ответ
-bool test(struct coeffs EQ, struct ans otvet)
+void test()
 {
-    int id0 = num_sol(EQ);
-    struct ans otvetik = answer(EQ, id0);
-    if ((otvet.id == otvetik.id) && (otvet.x1 == otvetik.x1) && (otvet.x2 == otvetik.x2)) return true;
-    return false;
+    struct coeffs cf[] =
+    {
+        {1, 5, 6},
+        {1, -2, 1},
+        {0, 0, 0},
+        {0, 0, 4},
+        {0, 6, 36},
+        {100, 1, 100},
+    } ;
+
+    struct ans otvety[] =
+    {
+        {2, -3, -2},
+        {1, 1, ZERO},
+        {4, ZERO, ZERO},
+        {5, ZERO, ZERO},
+        {3, -6, ZERO},
+        {0, ZERO, ZERO},
+    };
+
+    for (int i = 0;i < 6;++i)
+    {
+        double D = Discriminant(cf[i]);
+        int id0 = num_sol(cf[i]);
+        struct ans otvetik = answer(cf[i], id0);
+        if (otvety[i].id != 2)
+        {
+            if ((otvety[i].id == otvetik.id) && (otvety[i].x1 == otvetik.x1) && (otvety[i].x2 == otvetik.x2)) printf("Тест %d пройден\n", i + 1);
+        }
+        if (otvety[i].id == 2)
+        {
+            if (OK(cf[i], otvety[i].x1) && OK(cf[i], otvety[i].x2) && !(isnull(otvety[i].x1, otvety[i].x2))) printf("Тест %d пройден\n", i + 1);
+            else printf("Тест %d не пройден ");
+        }
+    }
 }
 
 #endif // TESTIK_H_INCLUDED
