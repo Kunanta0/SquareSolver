2f4ee60c53d627f25af1e08dfc3163b0cc6d32ef
Kunanta0 <faxim2020@yandex.ru>
2026-08-23 20:00:19 +0300

    Update output.h

--- output.h
+++ output.h
@@ -56,3 +56,30 @@ void printans(struct ans otvet, FILE* fp)
         break;
     }
 }
+
+//меню, которое запрашивает у пользователя запуск программы снова
+int menu()
+{
+    char ch = '\0';
+    bool notok = true;
+    int n = 0;
+
+    while(notok)
+    {
+        int cor = scanf("%d", &n);
+        if (cor != 1 || (ch = getchar()) != '\n')
+        {
+            while ((ch = getchar()) != '\n') continue;
+            printf("Неправильный ввод, введите 0, либо 1, либо 2: ");
+        }
+        else
+        {
+            if (n != 0 && n != 1 && n != 2) printf("Неправильный ввод, введите 0, либо 1, либо 2: ");
+            else notok = false;
+        }
+    }
+
+    return n;
+}
+
+#endif // OUTPUT_H_INCLUDED
