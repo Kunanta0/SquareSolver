40b421ae5702c640f1f2eb2a4f18f8ed54afecfe
Kunanta0 <faxim2020@yandex.ru>
2026-08-23 19:46:29 +0300

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <locale.h>
 #include <math.h>
+#include <windows.h>
 #include "input.h"
 #include "output.h"
 #include "myassert.h"
 #include "testik.h"
+#include "log.h"
 
 //0 - нет решения квадратного уравнения
 //1 - 1 решение квадратного уравнения
@@ -17,29 +20,45 @@
 
 int main(int argc, char* argv[])
 {
+    time_t now = time(NULL);
+    char* time_str = ctime(&now);
+    time_str[strcspn(time_str, "\n")] = 0;
+    FILE* fp = NULL;
+    fp = fopen("log.txt", "a");
     setlocale(LC_ALL, "Russian");
 
     while (true)
     {
         printf("Введите 0, чтобы завершить программу, 1, чтобы решать уравнение, 2, чтобы запустить тесты: ");
         int check = menu();
-        if (check == 0) break;
+        if (check == 0)
+        {
+            write_log(fp, time_str, LOG_INFO, PR_VAR(check, d));
+            break;
+        }
         else if (check == 1)
         {
+            write_log(fp, time_str, LOG_INFO, PR_VAR(check, d));
             printf(YELLOW "Квадратное уравнение имеет вид ax^2 + bx + c = 0\n" RESET);
 
             struct coeffs EQ =
             {
-                asknum('a'),
-                asknum('b'),
-                asknum('c'),
+                asknum('a', fp),
+                asknum('b', fp),
+                asknum('c', fp),
             };
             int id = num_sol(EQ);
+            write_log(fp, time_str, LOG_INFO, PR_VAR(id, d));
             struct ans otvet = answer(EQ, id);
-            printans(otvet);
+            printans(otvet, fp);
         }
-        else if (check == 2) test(argc, argv);
+        else if (check == 2)
+        {
+            write_log(fp, time_str, LOG_INFO, PR_VAR(check, d));
+            test(argc, argv, fp);
+        }
     }
     printf(YELLOW "Пока!" RESET);
+    fclose(fp);
     return 0;
 }
