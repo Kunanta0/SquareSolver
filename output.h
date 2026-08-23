d0a5d8e3792920ddf78e48d4edc6a00139a02550
Kunanta0 <faxim2020@yandex.ru>
2026-08-23 19:48:37 +0300

    Update output.h

--- output.h
+++ output.h
@@ -10,13 +10,17 @@
 #define THINKING "Думаю..."
 
 #include "maths.h"
+#include "log.h"
 
-void printans(struct ans);
+void printans(struct ans, FILE*);
 int menu();
 
 //выводит ответ
-void printans(struct ans otvet)
+void printans(struct ans otvet, FILE* fp)
 {
+    time_t now = time(NULL);
+    char* time_str = ctime(&now);
+    time_str[strcspn(time_str, "\n")] = 0;
     printf(THINKING);
     Sleep(2000);
     for (int i = 0;i < strlen(THINKING); ++i) printf("\b");
@@ -24,48 +28,31 @@ void printans(struct ans otvet)
     {
     case SQUARE_0:
         printf(YELLOW "Квадратное уравнение не имеет действительных корней, так как дискриминант меньше нуля\n\n" RESET);
+        write_log(fp, LOG_INFO, PR_VAR(otvet.id,d));
         break;
     case SQUARE_1:
         printf(YELLOW "1 решение, так как дискриминант равен нулю: %lg\n\n" RESET, otvet.x1);
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.id,d));
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.x1, lg));
         break;
     case SQUARE_2:
         printf(YELLOW "2 решения, так как дискриминант больше нуля: %lg и %lg\n\n" RESET, otvet.x1, otvet.x2);
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.id,d));
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.x1, lg));
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.x2, lg));
         break;
     case LINE:
         printf(YELLOW "Линейное уравнение, так как коэффициент a = 0, корень: %lg\n\n" RESET, otvet.x1);
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.id,d));
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.x1, lg));
         break;
     case ALWAYS_TRUE:
         printf(YELLOW "Бесконечное количество корней, так как все коэффициенты равны нулю (0 = 0)\n\n" RESET);
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.id,d));
         break;
     case ALWAYS_FALSE:
         printf(YELLOW "Корней нет, противоречие, так как все коэффициенты кроме c равны нулю, (c != 0)\n\n" RESET);
+        write_log(fp, time_str, LOG_INFO, PR_VAR(otvet.id,d));
         break;
     }
 }
-
-//меню, которое запрашивает у пользователя запуск программы снова
-int menu()
-{
-    char ch = '\0';
-    bool notok = true;
-    int n = 0;
-
-    while(notok)
-    {
-        int cor = scanf("%d", &n);
-        if (cor != 1 || (ch = getchar()) != '\n')
-        {
-            while ((ch = getchar()) != '\n') continue;
-            printf("Неправильный ввод, введите 0, либо 1, либо 2: ");
-        }
-        else
-        {
-            if (n != 0 && n != 1 && n != 2) printf("Неправильный ввод, введите 0, либо 1, либо 2: ");
-            else notok = false;
-        }
-    }
-
-    return n;
-}
-
-#endif // OUTPUT_H_INCLUDED
