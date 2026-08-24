24553bae9c4b2c3f0bd03c2251705c5f560cd4ed
Kunanta0 <faxim2020@yandex.ru>
2026-08-24 14:29:57 +0300

    Update myassert.h

--- myassert.h
+++ myassert.h
@@ -9,12 +9,7 @@
 if (!(exp))\
 {\
     fprintf(stderr, RED "Ошибка: %s в файле %s, в строке %d, в функции %s\n" RESET,#exp, __FILE__, __LINE__, __func__);\
-    write_log(fp, time_str, LOG_ERROR, "ERROR: %s in file %s, in line %d, in function %s\n" RESET,#exp, __FILE__, __LINE__, __func__);\
     exit(1);\
-}\
-else\
-{\
-    write_log(fp, time_str, LOG_INFO, "Work with file has been successfully finished");\
 }
 
 #endif // MYASSERT_H_INCLUDED
