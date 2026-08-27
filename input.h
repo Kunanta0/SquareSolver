99dd067f96ac6a6a6cd2303ce8862e5b7d8a4d01
Kunanta0 <faxim2020@yandex.ru>
2026-08-27 17:50:39 +0300

    Update input.h

--- input.h
+++ input.h
@@ -1,7 +1,7 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 
-const int MAX_SIZE = 100;
+const unsigned int MAX_SIZE = 1e5;
 double asknum(char, FILE*);
 double check_input(FILE* );
 
@@ -34,7 +34,7 @@ double check_input(FILE* flog)
         input[strcspn(input, "\n")] = 0;
         if (strlen(input) == 0)
         {
-            printf("Error: empty input. Enter 0, or 1, or 2: ");
+            printf("Error: empty input, enter a number: ");
             #ifdef DEBUG
             write_log(flog, LOG_INFO, "wrong users's input\n");
             #endif // DEBUG
@@ -44,7 +44,7 @@ double check_input(FILE* flog)
         number = strtod(input, &endptr);
         if (*endptr != '\0')
         {
-            printf("Error: \"%s\" is not a number. Enter 0, or 1, or 2: ", input);
+            printf("Error: \"%s\" is not a number. Enter a number: ", input);
             #ifdef DEBUG
             write_log(flog, LOG_INFO, "wrong users's input\n");
             #endif // DEBUG
