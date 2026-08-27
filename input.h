270cfbf2bde7fdd53a3bc3897ad8df59773fb932
Kunanta0 <faxim2020@yandex.ru>
2026-08-27 13:46:40 +0300

    Update input.h

--- input.h
+++ input.h
@@ -1,36 +1,57 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 
+const int MAX_SIZE = 100;
 double asknum(char, FILE*);
+double check_input(FILE* );
 
 //запрашивает коэффициенты квадратного уравнения
 double asknum(char coeff, FILE* fp)
 {
-    double n = 0;
-    char ch = '\0';
+    printf("Enter coefficient %c: ", coeff);
 
-    printf("Введите коэффициент %c: ", coeff);
+    return check_input(fp);
+}
 
-    do
+//проверяет корректность ввода
+double check_input(FILE* flog)
+{
+    char input[MAX_SIZE] = {};
+    char* endptr = NULL;
+    double number = 0;
+
+    while(true)
     {
-        int coeff_entered = scanf("%lg", &n);
-        if (coeff_entered != 1 || (ch = getchar()) != '\n')
+        if (fgets(input, sizeof(input), stdin) == NULL)
         {
-            printf("\"");
-            if (coeff_entered == 1) printf("%lg", n);
-            putchar(ch);
-            while ((ch = getchar()) != '\n') putchar(ch);
+            printf("Input error\n");
             #ifdef DEBUG
-            write_log(fp, LOG_INFO, "wrong users's input\n");
-            #endif
-            printf("\" не является числом, введите число: ");
+            write_log(flog, LOG_INFO, "wrong users's input\n");
+            #endif // DEBUG
+            continue;
         }
-        else break;
-    } while(true);
-    #ifdef DEBUG
-    write_log(fp, LOG_INFO, "coefficient has been being inputted success\n");
-    #endif
-    return n;
+
+        input[strcspn(input, "\n")] = 0;
+        if (strlen(input) == 0)
+        {
+            printf("Error: empty input. Enter 0, or 1, or 2: ");
+            #ifdef DEBUG
+            write_log(flog, LOG_INFO, "wrong users's input\n");
+            #endif // DEBUG
+            continue;
+        }
+
+        number = strtod(input, &endptr);
+        if (*endptr != '\0')
+        {
+            printf("Error: \"%s\" is not a number. Enter 0, or 1, or 2: ", input);
+            #ifdef DEBUG
+            write_log(flog, LOG_INFO, "wrong users's input\n");
+            #endif // DEBUG
+            continue;
+        }
+        return number;
+    }
 }
 
 #endif // INPUT_H_INCLUDED
