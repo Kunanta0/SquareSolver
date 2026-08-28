edf87270fdde9f3a25d4941ab5b099b7517bfd5e
Kunanta0 <faxim2020@yandex.ru>
2026-08-28 20:33:30 +0300

    Update testik.h

--- testik.h
+++ testik.h
@@ -8,40 +8,31 @@
 
 /**
 *this function runs tests
-*\param[in] argv_i name of test file
+*\param[in] filename name of test file
 *\param[out] flog log file
 */
 void test(char*, FILE*);
 
 /**
 *this function initialize test file, returns number of its lines
-*\param[in] argv_i name of test file
+*\param[in] filename name of test file
 *\param[in,out] file_test test file
 *\return number of lines
 */
 int test_init(char*, FILE**);
 
 //function for tests
-void test(char* argv_i, FILE* flog)
+void test(char* filename, FILE* flog)
 {
     FILE* test_file = NULL;
-    int num_strs = 1;
-    num_strs = test_init(argv_i, &test_file);
-
-    struct coeffs* cf = (struct coeffs*)calloc(num_strs, sizeof(struct coeffs));
-    struct ans* answers = (struct ans*)calloc(num_strs, sizeof(struct ans));
-
-    #ifdef DEBUG
-    write_log(flog, LOG_INFO, "func calloc took %d numbers memory of struct ans\n", num_strs);
-    write_log(flog, LOG_INFO, "func calloc took %d numbers memory of struct coeffs\n", num_strs);
-    #endif
+    int num_strs = test_init(filename, &test_file);
 
     for (unsigned int i = 0;i < num_strs; ++i)
     {
         MyAssert(0 <= i && i < num_strs);
-        struct ans ans_ref = answers[i];
-        struct coeffs coefficients = cf[i];
-        fscanf(test_file, "%lg %lg %lg %d %lg %lg",&coefficients.a, &coefficients.b, &coefficients.c, &ans_ref.id, &ans_ref.x1, &ans_ref.x2);
+        struct ans ans_ref = {};
+        struct coeffs coefficients = {};
+        fscanf(test_file, "%lg %lg %lg %d %lg %lg", &coefficients.a, &coefficients.b, &coefficients.c, &ans_ref.id, &ans_ref.x1, &ans_ref.x2);
         struct ans ans_prog = answer(coefficients);
         if (ans_ref.id == ans_prog.id && (issame(ans_ref.x1, ans_prog.x1) && issame(ans_ref.x2, ans_prog.x2)) || (issame(ans_ref.x1, ans_prog.x2) && issame(ans_ref.x2, ans_prog.x1)))
         {
@@ -58,18 +49,16 @@ void test(char* argv_i, FILE* flog)
             #endif
         }
     }
-    free(cf);
-    free(answers);
     if (fclose(test_file) != 0) printf("Error of closing file\n");
     printf("\n");
 }
 
 //returns number of lines in test, accepts pointer to file, which fills
-int test_init(char* argv_i, FILE** file_test)
+int test_init(char* filename, FILE** file_test)
 {
     int strs = 1;
     char ch = '0';
-    if ((*file_test = fopen(argv_i, "r")) == NULL) printf("Error of closing file\n");
+    if ((*file_test = fopen(filename, "r")) == NULL) printf("Error of opening file\n");
     while ((ch = getc(*file_test)) != EOF)
     {
         if (ch == '\n') ++strs;
