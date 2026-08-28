d60a3e574afa2b052dce9ad324dac12cb4dfc558
Kunanta0 <faxim2020@yandex.ru>
2026-08-28 14:37:07 +0300

    Update myassert.h

--- myassert.h
+++ myassert.h
@@ -1,11 +1,12 @@
 #ifndef MYASSERT_H_INCLUDED
 #define MYASSERT_H_INCLUDED
 
-//свой assert()
+///this is realization of my assert
+//my assert()
 #define MyAssert(exp)\
 if (!(exp))\
 {\
-    fprintf(stderr, RED "Ошибка: %s в файле %s, в строке %d, в функции %s\n" RESET,#exp, __FILE__, __LINE__, __func__);\
+    fprintf(stderr, RED "Error: %s in file %s, in lines %d, in function %s\n" RESET,#exp, __FILE__, __LINE__, __func__);\
     exit(1);\
 }
 
