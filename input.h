12ffbdc1c004a399981add6719bd5b8c521ae690
Kunanta0 <faxim2020@yandex.ru>
2026-08-28 19:42:52 +0300

    Update input.h

--- input.h
+++ input.h
@@ -1,18 +1,34 @@
+/**
+*\file
+*\brief Header with input functions
+*/
+
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 
+/**
+*accepts char coefficient and ask it
+*\param[out] coeff char of coefficient
+*\param[out] flog log file
+*\return double number, which users entered
+*/
 double asknum(char, FILE*);
+
+/**
+*accepts log file to write there user's number, while he won't enter good input
+*\param[out] flog log file
+*\return number after checking
+*/
 double check_input(FILE* );
 
 //asks coefficients of quadratic equation
-double asknum(char coeff, FILE* flog)///<function asknum() accepts char coefficient and ask it
+double asknum(char coeff, FILE* flog)
 {
     printf("Enter coefficient %c: ", coeff);
 
     return check_input(flog);
 }
 
-///function check_input() accepts log file to write there user's number, while he won't enter good input
 //check input
 double check_input(FILE* flog)
 {
@@ -23,21 +39,11 @@ double check_input(FILE* flog)
         int return_scanf = scanf("%lg", &number);
         if (return_scanf != 1 || (ch = getchar()) != '\n')
         {
-            if (return_scanf == 1)
-            {
-                printf("\"%lg", number);
-                putchar(ch);
-                while((ch = getchar()) != '\n') putchar(ch);
-                printf("\" - wrong input, enter a number: ");
-                continue;
-            }
-            else
-            {
-                printf("\"");
-                while((ch = getchar()) != '\n') putchar(ch);
-                printf("\" - wrong input, enter a number: ");
-                continue;
-            }
+            printf("\"");
+            if (return_scanf == 1) putchar(ch);
+            while((ch = getchar()) != '\n') putchar(ch);
+            printf("\" is not a number input, enter a number: ");
+            continue;
         }
         #ifdef DEBUG
         write_log(flog, LOG_INFO, PR_VAR(number, d));
